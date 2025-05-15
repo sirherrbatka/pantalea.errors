69df82930acd6804e04700984b46c169eb197df6
Marek Kochanowicz <marek-kochanowicz@posteo.com>
2025-05-15 17:33:48 +0200

    Added *style*

--- source/code.lisp
+++ source/code.lisp
@@ -6,25 +6,77 @@
 
 (defparameter *cause* nil)
 
-(defparameter *nested-level* 0)
+(defparameter *label* 0)
+
+(defparameter *style* :plain)
 
 (defgeneric chained-error-cause (error))
 
+(defgeneric chained-error-text (error))
+(defgeneric chained-error-arguments (error))
+
+(defun json-escape-string (string out)
+  "Escape STRING for safe inclusion in JSON."
+  (loop for ch across string do
+    (case ch
+      (#\" (write-string "\\\"" out))
+      (#\\ (write-string "\\\\" out))
+      (#\Newline (write-string "\\n" out))
+      (#\Return (write-string "\\r" out))
+      (#\Tab (write-string "\\t" out))
+      (#\Backspace (write-string "\\b" out))
+      (#\Page (write-string "\\f" out))
+      (t (if (or (char<= #\Space ch #\~))
+             (write-char ch out)
+             ;; Escape non-ASCII control characters as \uXXXX
+             (format out "\\u~4,'0X" (char-code ch)))))))
+
 (defun format-cause (error stream)
-  (let ((*nested-level* (1+ *nested-level*)))
-    (alexandria:when-let ((cause (chained-error-cause error)))
-      (format stream "~%")
-      (format stream "[~a] Caused by:~%~a" *nested-level* cause))))
+  (alexandria:when-let ((cause (chained-error-cause error)))
+    (alexandria:eswitch (*style*)
+      (:plain
+       (format stream "~%")
+       (format stream "(~a) Caused by:~%~a" *label* cause))
+      (:json
+       (if (typep cause 'chained-error)
+           (format stream ", \"cause\": ~a" cause)
+           (progn
+             (format stream ", \"cause\": \"" )
+             (json-escape-string (format nil "~a" cause) stream)
+             (format stream "\"")))))))
 
 (defun report-chained-error (condition stream)
-  (format stream "Error of type ~a was signaled." (type-of condition))
-  (format-cause condition stream))
+  (let ((*label* (1+ *label*)))
+    (alexandria:eswitch (*style*)
+      (:plain
+       (format stream "Error ~a." (type-of condition))
+       (alexandria:when-let ((text (chained-error-text condition)))
+         (format stream " ")
+         (apply #'format text (chained-error-arguments condition)))
+       (format-cause condition stream))
+      (:json
+       (format stream "{\"error_type\": \"" )
+       (json-escape-string (symbol-name (type-of condition)) stream)
+       (format stream "\"")
+       (alexandria:when-let ((text (chained-error-text condition)))
+         (format stream ", \"text\": \"")
+         (json-escape-string (apply #'format stream text (chained-error-arguments condition))
+                             stream)
+         (format stream "\""))
+       (format-cause condition stream)
+       (format stream "}")))))
 
 (define-condition chained-error (cl:error)
   ((%cause :initarg :cause
-           :reader chained-error-cause))
+           :reader chained-error-cause)
+   (%text :initarg :text
+          :reader chained-error-text)
+   (%arguments :initarg :arguments
+               :reader chained-error-arguments))
   (:report report-chained-error)
-  (:default-initargs :cause *cause*))
+  (:default-initargs :cause *cause*
+                     :arguments nil
+                     :text nil))
 
 (defgeneric chained-error-root-cause (cl:error)
   (:method ((error cl:error))
@@ -61,11 +113,17 @@
            (,!impl)))))
 
 (defun make-chained (type &rest args)
-  (if *chained-enabled*
+  (if *chain-enabled*
       (apply #'make-condition (link-error-name type) args)
       (apply #'make-condition type args)))
 
-(defmacro !!! (type &rest args)
-  `(if *chained-enabled*
-      (cl:error  ',(link-error-name type) ,@args)
-      (cl:error ',type ,@args)))
+(defmacro !!! (type text-args &rest args)
+  `(if *chain-enabled*
+      (cl:error  ',(link-error-name type)
+                 :text ,(first args)
+                 :arguments (list ,@(rest text-args))
+                 ,@args)
+      (cl:error ',type
+                 :text ,(first args)
+                 :arguments (list ,@(rest text-args))
+                ,@args)))
--- source/package.lisp
+++ source/package.lisp
@@ -6,6 +6,7 @@
    #:chained-error-cause
    #:chained-error-root-cause
    #:format-cause
+   #:*style*
    #:!!!
    #:make-chained
    #:with-link))
